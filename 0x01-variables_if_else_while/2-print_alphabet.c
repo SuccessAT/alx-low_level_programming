7dd630675d1b19bf4c67ff7dab3d403401152d25
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 14:07:17 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main code prints a-z
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	int a;
+
+	for(a='a'; a<='z'; a++)
+	{
+		putchar(a);
+	}
+
+	return (0);
+}
