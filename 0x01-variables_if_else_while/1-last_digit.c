8700405a1378133e5f2ea9e2c7b0bbf1129df046
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 09:54:54 -0700

    exercise #1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the main code analyzes the last digit 
+ * of an integer with 5 and outputs the result
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* Beginning of my code */
+	
+	int a;
+       
+	a = n % 10;
+
+	if (a > 5 )
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else if (a < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
+	/* End of my code */
+	return (0);
+}
