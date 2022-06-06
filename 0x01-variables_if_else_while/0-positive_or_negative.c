54e08d16abaa7d027e484a3107c5fd85b7106c56
SuccessAT <agboyinusuccess@gmail.com>
2022-06-05 21:00:23 -0700

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main: compares whether a random number is positive or negative
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
