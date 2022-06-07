f2d8c5000953e53bacbea20ad1e5498309bb3953
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 21:18:17 -0700

    working sample 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main code prints 0-9
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf(a);
+	}
+	printf('\n');
+
+	return (0);
+}
