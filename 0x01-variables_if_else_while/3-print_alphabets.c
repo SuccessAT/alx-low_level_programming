e0dcb9610ac2b385c4cdee369740c301b34a670c
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 20:44:05 -0700

    working sample putchar 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main code prints a-z
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		if (a == 'z')
+		{
+			a = 'A';
+			for (a = 'A'; a <= 'Z'; a++)
+			{
+				putchar(a);
+				if (a == 'Z')
+				{
+					putchar('\n');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
