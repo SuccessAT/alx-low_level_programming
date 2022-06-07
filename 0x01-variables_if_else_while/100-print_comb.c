b0ab9128001df77070d93509f7597491dba27589
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 22:27:54 -0700

    extra 1

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the main code prints a-z
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; a <= '9'; a++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a = '8' && b = '9')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
