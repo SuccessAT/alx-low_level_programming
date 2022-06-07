b794899667f93f0ae187edb0645af6b089cb2030
SuccessAT <agboyinusuccess@gmail.com>
2022-06-06 22:54:46 -0700

    new directory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - the main code goes here
+ *
+ * Return: The function returns zero(0)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
