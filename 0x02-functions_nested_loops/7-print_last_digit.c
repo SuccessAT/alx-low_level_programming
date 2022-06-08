0ff449a1938eeef75ab61fc3095cd9cd826670cb
SuccessAT <agboyinusuccess@gmail.com>
2022-06-07 22:01:23 -0700

    forge ahead

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digits - the main function goes here
+ *
+ * @c: The parameter to be checked
+ *
+ * Return: The function returns an integer 1 oe 0
+ */
+
+int print_last_digit(int c)
+{
+	int retval = (c % 10);
+
+	return (retval);
+}
