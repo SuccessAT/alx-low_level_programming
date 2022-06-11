0cde87c40126c1301855dec697d350fb5546fc0b
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 19:22:13 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _idigit - this is the original function
+ * _idigit - this is a c  function
  * @c: an argument we pass
  * Return: the value returned by the program is 0 or 1
  */
