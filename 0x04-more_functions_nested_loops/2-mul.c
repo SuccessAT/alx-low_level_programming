56c40d8daf566f44b4aed5673120f2f3269a8f3f
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 19:31:45 -0700

    muller

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - this is a c function
+ * @a: an argument we pass
+ * @b: another argument we pass
+ * Return: the product of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
