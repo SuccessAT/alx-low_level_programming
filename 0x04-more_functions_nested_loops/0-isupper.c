3c24de46bd46189e835af952a20aedb06b07e1e4
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 19:09:32 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - this is the original function
+ * @c: an argument we pass
+ * Return: the value returned by the program is 0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
