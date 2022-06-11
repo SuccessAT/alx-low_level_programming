5cdbf25ea7e814924431b008d3c313d5280d5225
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 21:02:21 -0700

    trials

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@ void print_line(int n)
 {
 	int j;
 
-	if (n <= 0)
+	for (j = 0; j < n; j++)
 	{
-		_putchar('\n');
-	}
+		if (n <= 0)
+		{
+			break;
+		}
 
-	for (j = 0; j <= n; j++)
-	{
 		_putchar('_');
 	}
 	_putchar('\n');
