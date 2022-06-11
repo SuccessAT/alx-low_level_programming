95e47693a8c496f13abec66bb4c3a37cbe44e54a
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 20:24:23 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - this is a c function
+ * @n: argument we pass
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (j <=0)
+		_putchar('\n');
+
+	for (j = 0; j <= n; j++)
+		_putchar('_');
+
+	_putchar('\n');
+}
