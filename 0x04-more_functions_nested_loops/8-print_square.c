4f3ef360c23f0938e670d4eff04f808787542151
SuccessAT <agboyinusuccess@gmail.com>
2022-06-10 21:40:52 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,10 +15,9 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 0; j < size; j++)
 				_putchar(35);
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
