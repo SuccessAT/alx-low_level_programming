cd69717166aab6c2ada01b1d10d88f3cb7e9f381
SuccessAT <agboyinusuccess@gmail.com>
2022-06-13 20:21:00 -0700

    going

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /*
- * swap_int - main function to swap two numbers
+ * swap_int -> main function to swap two numbers
  * @a: parameter 1
  * @b: parameter 2
  */
+
 void swap_int(int *a, int *b)
 {
 	int hold;
