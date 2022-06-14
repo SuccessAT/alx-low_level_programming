ea45153672abf5066899b82da45474a093703639
SuccessAT <agboyinusuccess@gmail.com>
2022-06-13 21:51:18 -0700

    gone

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - main function goes in here
+ * @str: the string parameter
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
