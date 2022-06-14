38d01c056339a6b9ef1e404bb3593ad29b2eda43
SuccessAT <agboyinusuccess@gmail.com>
2022-06-13 20:53:26 -0700

    going

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function goes in here
+ * @str: parameter passed into function
+ */
+
+void _puts(char *str)
+{
+	for (; *s != '\0'; s++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
