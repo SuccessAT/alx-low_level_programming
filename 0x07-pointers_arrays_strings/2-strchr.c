1d6494770a9268156b437e8df48187ac9157309c
SuccessAT <agboyinusuccess@gmail.com>
2022-06-20 16:25:22 -0700

    epp me epp me

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - a clone of the strchr function
+ * @s: string to be checked
+ * @c: character we check for
+ *
+ * Return: a pointer to c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *result;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			result = s[i];
+		else
+			result = '\0';
+
+	return (result);
+}
