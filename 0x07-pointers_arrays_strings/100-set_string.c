1c29e910120313ca79557a499e00252ab9617da5
SuccessAT <agboyinusuccess@gmail.com>
2022-06-20 20:38:10 -0700

    check

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * setstring - a r function
+ * @s: string to be checked
+ * @to: character we check for
+ *
+ */
+void set_string(char **s, char *to)
+{
+	s = &to;
+}
