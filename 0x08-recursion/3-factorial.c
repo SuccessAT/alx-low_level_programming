d949fbcf65b732f9b07717f5fe342249d354dcad
SuccessAT <agboyinusuccess@gmail.com>
2022-06-22 22:06:05 -0700

    we go along

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,13 +2,21 @@
 
 /**
  * factorial - a c function
- * n: a parameter
+ * @n: a parameter
  *
  * Return: the factorial of a number n
  */
 
 int factorial(int n)
 {
+	unsigned int i = 0;
+
+	if (n < 0)
+	{
+		i = -1;
+		return(i);
+	}
+
 	if (n == 0)
 	{
 		return(1);
