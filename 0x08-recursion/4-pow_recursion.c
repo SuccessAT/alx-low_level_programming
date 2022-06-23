ffdbd36e4dee1cedd41460c43c3f03fa841bcba8
SuccessAT <agboyinusuccess@gmail.com>
2022-06-23 09:33:34 +0100

    keep moving

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a c function
+ * @x: a parameter representing the number to be exponentiated
+ * @y: a second parameter representing the exponent
+ *
+ * Return: an integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	unsigned int i = 0;
+
+	if (y < 0)
+	{
+		i = -1;
+		return (i);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	y--;
+	return (x * _pow_recursion(x));
+}
