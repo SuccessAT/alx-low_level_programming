0f29840d4934cbe7c8a75b3a3208397ece5078ae
SuccessAT <agboyinusuccess@gmail.com>
2022-06-24 13:34:05 -0700

    starts

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * @argc: argument count
  * @argv: argument vector
  *
- * Result: always 0
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s \n", argv[0]);
 	return (0);
 }
