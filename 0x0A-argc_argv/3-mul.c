996e30b494abd2b6cff861117d687e0e465b2362
SuccessAT <agboyinusuccess@gmail.com>
2022-06-25 00:13:10 +0100

    closer

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,23 +10,22 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, result, ret;
-
-	sscanf(argv[1], "%d", &i);
-	sscanf(argv[2], "%d", &j);
-
-	result = i * j;
-
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		ret = 1;
+		int i, j, result;
+
+		sscanf(argv[1], "%d", &i);
+		sscanf(argv[2], "%d", &j);
+
+		result = i * j;
+
+		printf("%d\n", result);
+		return (0);
 	}
+	
 	else
 	{
-		printf("%d\n", result);
-		ret = 0;
+		printf("Error\n");
+		return (1);
 	}
-
-	return (ret);
 }
