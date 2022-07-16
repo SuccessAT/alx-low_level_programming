79953cf98a0d70b9d25afa49a2603da2b450a16f
SuccessAT <agboyinusuccess@gmail.com>
2022-07-16 05:54:22 +0100

    slowly but surely

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef LISTS_H_INCLUDED
 #define LISTS_H_INCLUDED
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
