b420e8d1c7c4c361bf75c2acd90a17277a759ab8
SuccessAT <agboyinusuccess@gmail.com>
2022-07-18 23:31:49 +0100

    throttle

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_listint - function that prints the contents of a linked list
+ * @h: struct parameter
+ * Return: the number of elements in the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
