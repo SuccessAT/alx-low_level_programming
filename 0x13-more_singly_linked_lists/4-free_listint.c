6dbf473651872df2c212c1e5dd92582d919bf82d
SuccessAT <agboyinusuccess@gmail.com>
2022-07-19 14:05:45 +0100

    testing

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * listint_len - function that prints the legth of a linked list
+ * @h: struct parameter
+ * Return: the number of elements in the linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = current->next;
+		free(current);
+	}
+}
