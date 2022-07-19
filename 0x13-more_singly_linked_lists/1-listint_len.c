d0636b641a81b2d5153bcfbb127bca991873eb5d
SuccessAT <agboyinusuccess@gmail.com>
2022-07-19 02:49:17 +0100

    counter

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * listint_len - function that prints the legth of a linked list
+ * @h: struct parameter
+ * Return: the number of elements in the linked list
+ */
+
+ize_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
